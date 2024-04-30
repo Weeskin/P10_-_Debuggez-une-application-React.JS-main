c07959e44dfbd0d8b96c0fb553a04794c7fc5940
Pierre Sourice <pierre.sourice38@gmail.com>
2024-04-30 14:45:14 +0200

    Erreur EvenCard fix, erreur Logo fix, erreur Sliders en cours

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -4,42 +4,51 @@ import { getMonth } from "../../helpers/Date";
 import "./style.scss";
 
 const EventCard = ({
-  imageSrc,
-  imageAlt,
-  date = new Date(),
-  title,
-  label,
-  small = false,
-  ...props
+	imageSrc = "",
+	imageAlt,
+	date = new Date(),
+	title = "",
+	label,
+	small = false,
+	...props
 }) => (
-    <div
-      data-testid="card-testid"
-      className={`EventCard${small ? " EventCard--small" : ""}`}
-      {...props}
-    >
-      <div className="EventCard__imageContainer">
-        <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
-        <div className="EventCard__label">{label}</div>
-      </div>
-      <div className="EventCard__descriptionContainer">
-        <div className="EventCard__title">{title}</div>
-        <div className="EventCard__month">{getMonth(date)}</div>
-      </div>
-    </div>
-  );
+	<div
+		data-testid="card-testid"
+		className={`EventCard${small ? " EventCard--small" : ""}`}
+		{...props}
+	>
+		<div className="EventCard__imageContainer">
+			<img
+				data-testid="card-image-testid"
+				src={imageSrc}
+				alt={imageAlt}
+			/>
+			<div className="EventCard__label">{label}</div>
+		</div>
+		<div className="EventCard__descriptionContainer">
+			<div className="EventCard__title">{title}</div>
+			<div className="EventCard__month">{getMonth(date)}</div>
+		</div>
+	</div>
+);
 
 EventCard.propTypes = {
-  imageSrc: PropTypes.string.isRequired,
-  imageAlt: PropTypes.string,
-  date: PropTypes.instanceOf(Date).isRequired,
-  title: PropTypes.string.isRequired,
-  small: PropTypes.bool,
-  label: PropTypes.string.isRequired,
+	imageSrc: PropTypes.string,
+	imageAlt: PropTypes.string,
+	date: PropTypes.instanceOf(Date),
+	title: PropTypes.string,
+	small: PropTypes.bool,
+	label: PropTypes.string
 };
 
 EventCard.defaultProps = {
-  imageAlt: "image",
-  small: false,
-}
+	imageSrc:
+		"../../../public/images/priscilla-du-preez-Q7wGvnbuwj0-unsplash1.png",
+	imageAlt: "image",
+	title: "Event title",
+	small: false,
+	date: "2021-01-01",
+	label: "new"
+};
 
 export default EventCard;
